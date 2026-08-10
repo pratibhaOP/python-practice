cda18ad1eb03364d48f811251528a53406bcb145
PratibhaOP <gangireddypratibha@gmail.com>
2026-08-10 17:21:06 -0400

    add loops, error handling, and division by zero check to calculator

--- 01-basics/calculator.py
+++ 01-basics/calculator.py
@@ -1,4 +1,29 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-result = num1 + num2
-print("The sum of", num1, "and", num2, "is:", result)
+while True:
+    try:
+        num1 = float(input("enter first number:"))
+        num2 = float(input("enter second number:"))
+        operation = input("enter second operation (+,-,*,/):")
+
+        if operation == '+':
+         result = num1 + num2
+        elif operation == '-':
+         result = num1 - num2
+        elif operation == '*':
+         result = num1 * num2
+        elif operation == '/':
+         if num2 !=0:
+           result = num1 / num2
+         else:
+           result = "error: division by zero"
+        else: 
+           result = " error: Invalid Operation"
+
+        print("The result is:", result)   
+
+    except ValueError: 
+      print("This is not a valid number. Try again")
+
+    again = input("Calculate again? (yes/no): ")
+    if again.lower() != "yes":
+        print("Goodbye!")
+        break
