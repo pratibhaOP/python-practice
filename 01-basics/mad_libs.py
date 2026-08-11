80749c1c0cc3d07a87dab17edfd0a70648c7c852
PratibhaOP <gangireddypratibha@gmail.com>
2026-08-11 13:33:51 -0400

    f- string

---
+++ 01-basics/mad_libs.py
@@ -0,0 +1,9 @@
+name = "dolly"
+adjective = "happy"
+animal = "dog"
+verb = "dance"
+place = "mountains"
+
+print(f"Once upon a time, there was a person named {name}.")
+print(f"They had a pet {adjective} {animal} who loved to {verb} all day.")
+print(f"One day, they both went on an adventure to {place} and never wanted to leave.")
