1767f72eec2de837ffa89466df52931266359545
PratibhaOP <gangireddypratibha@gmail.com>
2026-08-11 14:33:55 -0400

    random guessing

---
+++ 02-control-flow/number_guessing.py
@@ -0,0 +1,20 @@
+import random
+secret_number=7
+attempts = 0
+
+while True: 
+    try: 
+        guess = int(input(" guess a number between 1 to 100: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("too low, try again: ")
+        elif guess > secret_number:
+            print("too high, try again: ")
+        else: 
+            print(f"correct! you guess after {attempts} attempts.")
+            break
+    except ValueError:
+        print("thats not a valid number, try again")
+
+        
